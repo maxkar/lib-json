f01a051ab782266bbe71709489d72335b6ccfda4
maxkar <none@none>
2015-11-05 22:48:19 +0000

    * Added a readme describing intetnions behind this library * Initial type hierarchy is implemented and documented
    --HG--
    branch : universal-json

--- src/ru/maxkar/json/JsonException.scala
+++ src/ru/maxkar/json/JsonException.scala
@@ -1,4 +1,11 @@
 package ru.maxkar.json
 
-/** Exception in JSON handling. */
-class JsonException(msg : String) extends Exception(msg)
+import java.io.IOException
+
+/**
+ * Exception in JSON handling or processing. Json parsing or interpretation
+ * is the main source of these exceptions. A root cause is the incorrect data
+ * format of the incoming request hence the inheritance from the IO exception.
+ * Inability to access/generated json is a common kind of "input" error.
+ */
+class JsonException(msg : String) extends IOException(msg)
